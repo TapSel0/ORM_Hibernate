c2264c1207ecd1964878d0087dd91606bdb9ed16
TapSel0 <dvratskiy3@mail.ru>
2024-08-04 23:12:45 +0700

    Create hibernate.cfg.xml

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,8 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
-    implementation("org.hibernate:hibernate-core:5.4.32.Final")
+    // https://mvnrepository.com/artifact/org.hibernate/hibernate-core
+    implementation("org.hibernate:hibernate-core:6.5.2.Final")
     implementation("org.springframework:spring-context:5.3.8")
     implementation("javax.persistence:javax.persistence-api:2.2")
     implementation("mysql:mysql-connector-java:8.0.32")
